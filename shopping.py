0ec671a49aa2096b5c0234fe97e8c277598e2970
dBob <bobrovnykov@yahoo.com>
2025-02-24 20:42:02 +0700

    Load june fix

--- shopping.py
+++ shopping.py
@@ -64,7 +64,7 @@ def load_data(filename):
               'mar':2,
               'apr':3,
               'may':4,
-              'june':5,
+              'jun':5,
               'jul':6,
               'aug':7,
               'sep':8,
